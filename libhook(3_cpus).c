b61a899ba69dd4325cc5194890d46431aa3ec0f4
wasabbi <lee.yeb33@gmail.com>
2020-01-06 22:49:53 +0900

    123

--- libhook(3_cpus).c
+++ libhook(3_cpus).c
@@ -9,7 +9,7 @@ typedef int (*orig_pthread_create_ftype)(pthread_t *, const pthread_attr_t *, vo
 
 void* thread1 = 0x12345786;
 void* thread2 = 0x123457eb;
-void* thread3 = 0;
+void* thread3 = 0x0;
 
 int pthread_create(pthread_t *__restrict __newthread,
 			   const pthread_attr_t *__restrict __attr,
