5cdbd765507778302f56efa05c6c00590db09f7f
wasabbi <lee.yeb33@gmail.com>
2020-01-10 18:34:40 +0900

    123

--- libhook.c
+++ libhook.c
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <dlfcn.h>
 #include <pthread.h>
+#include <sys/syscall.h>
 
 typedef int (*orig_pthread_create_ftype)(pthread_t *, const pthread_attr_t *, void* (void *), void *);
 
@@ -13,10 +14,42 @@ void* thread3 = 0x0;
 
 static int flag1 = 0;
 
+struct mypara{
+    void *(*__orig_start_routine) (void *);
+    void *__orig_arg;
+};
+
+void *transition_func(void *arg){
+    void *(*__orig_start_routine) (void *);
+    void *__orig_arg;
+    struct mypara *mp;
+    mp = (struct mypara*)arg;
+    __orig_start_routine = mp->__orig_start_routine;
+    __orig_arg = mp->__orig_arg;
+
+    printf("[libhook.so] child gettid = %u\n", syscall(SYS_gettid));  
+    printf("[libhook.so] child pthread_self= %u\n", (unsigned int)pthread_self());    
+
+    if(__orig_start_routine == thread1){
+        printf("[libhook.so] manage_hw_bp has run\n");
+        system("./manage_hw_bp");
+        flag1 = 1;
+    }
+    else if(__orig_start_routine != thread1){
+        while(flag1 == 0){
+            //wait for manage_hw_bp
+        }
+    }
+
+    printf("[libhook.so] Transition_func started\n");
+    __orig_start_routine(__orig_arg);
+    printf("[libhook.so] Transition_func ended\n");
+}
+
 int pthread_create(pthread_t *__restrict __newthread,
-			   const pthread_attr_t *__restrict __attr,
-			   void *(*__start_routine) (void *),
-			   void *__restrict __arg){
+                    const pthread_attr_t *__restrict __attr,
+                    void *(*__start_routine) (void *),
+                    void *__restrict __arg){
 
     int ret;
 
@@ -24,64 +57,68 @@ int pthread_create(pthread_t *__restrict __newthread,
     orig_pthread_create = (orig_pthread_create_ftype)dlsym(RTLD_NEXT, "pthread_create");
 
     if(__start_routine == thread1){
-	if(flag1 == 0){
-		flag1 = 1;
-		printf("./manage_hw_bp has run\n");
-		system("./manage_hw_bp");
-	}
         cpu_set_t mask1;
         CPU_ZERO(&mask1);
         CPU_SET(0, &mask1);
         if (pthread_setaffinity_np(pthread_self(),sizeof(mask1),&mask1) < 0)                  //bind the thread1 to CPU0
             fprintf(stderr,"set thread affinity failed\n");
-        printf("Thread1 binds to CPU0\n");
-        ret = (*orig_pthread_create)(__newthread, __attr, __start_routine, __arg);
+        //printf("[libhook.so] Thread1 binds to CPU0\n");
 
-	cpu_set_t mask2;
+        struct mypara mp;
+        mp.__orig_start_routine = __start_routine;
+        mp.__orig_arg = __arg;
+        ret = (*orig_pthread_create)(__newthread, __attr, transition_func, &mp);
+
+        cpu_set_t mask2;
         CPU_ZERO(&mask2);
-        CPU_SET(2, &mask2);
-        if (pthread_setaffinity_np(pthread_self(),sizeof(mask2),&mask2) < 0)                  //bind the caller thread to CPU1
+        CPU_SET(3, &mask2);
+        if (pthread_setaffinity_np(pthread_self(),sizeof(mask2),&mask2) < 0)                  //bind the caller thread to CPU3
             fprintf(stderr,"set thread affinity failed\n");
-        printf("Caller Thread binds to CPU2\n");
+        //printf("[libhook.so] Caller Thread binds to CPU3\n");
     }
     else if(__start_routine == thread2){
-	if(flag1 == 0){
-		flag1 = 1;
-		printf("./manage_hw_bp has run\n");
-		system("./manage_hw_bp");
-	}
         cpu_set_t mask1;
         CPU_ZERO(&mask1);
         CPU_SET(1, &mask1);
         if (pthread_setaffinity_np(pthread_self(),sizeof(mask1),&mask1) < 0)                  //bind the thread2 to CPU1
             fprintf(stderr,"set thread affinity failed\n");
-        printf("Thread2 binds to CPU1\n");
-        ret = (*orig_pthread_create)(__newthread, __attr, __start_routine, __arg);
+        //printf("[libhook.so] Thread2 binds to CPU1\n");
 
-	cpu_set_t mask2;
+        struct mypara mp;
+        mp.__orig_start_routine = __start_routine;
+        mp.__orig_arg = __arg;
+        ret = (*orig_pthread_create)(__newthread, __attr, transition_func, &mp);
+
+        cpu_set_t mask2;
         CPU_ZERO(&mask2);
-        CPU_SET(2, &mask2);
-        if (pthread_setaffinity_np(pthread_self(),sizeof(mask2),&mask2) < 0)                  //bind the caller thread to CPU1
+        CPU_SET(3, &mask2);
+        if (pthread_setaffinity_np(pthread_self(),sizeof(mask2),&mask2) < 0)                  //bind the caller thread to CPU3
             fprintf(stderr,"set thread affinity failed\n");
-        printf("Caller Thread binds to CPU2\n");
+        //printf("[libhook.so] Caller Thread binds to CPU3\n");
     }
     else if(__start_routine == thread3){
-	if(flag1 == 0){
-		flag1 = 1;
-		printf("./manage_hw_bp has run\n");
-		system("./manage_hw_bp");
-	}
         cpu_set_t mask1;
         CPU_ZERO(&mask1);
         CPU_SET(2, &mask1);
         if (pthread_setaffinity_np(pthread_self(),sizeof(mask1),&mask1) < 0)                  //bind the thread3 to CPU2
+        fprintf(stderr,"set thread affinity failed\n");
+        //printf("[libhook.so] Thread3 binds to CPU2\n");
+        
+        struct mypara mp;
+        mp.__orig_start_routine = __start_routine;
+        mp.__orig_arg = __arg;
+        ret = (*orig_pthread_create)(__newthread, __attr, transition_func, &mp);
+
+        cpu_set_t mask2;
+        CPU_ZERO(&mask2);
+        CPU_SET(3, &mask2);
+        if (pthread_setaffinity_np(pthread_self(),sizeof(mask2),&mask2) < 0)                  //bind the caller thread to CPU3
             fprintf(stderr,"set thread affinity failed\n");
-        printf("Thread3 binds to CPU2\n");
-        ret = (*orig_pthread_create)(__newthread, __attr, __start_routine, __arg);
+        //printf("[libhook.so] Caller Thread binds to CPU3\n");
     }
     else{
         ret = (*orig_pthread_create)(__newthread, __attr, __start_routine, __arg);
     }
-    
+
     return ret;
 }
