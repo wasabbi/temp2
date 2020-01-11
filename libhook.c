796ea31503c6c724a65b23735227c8f86aa4d604
wasabbi <lee.yeb33@gmail.com>
2020-01-12 00:32:44 +0900

    123

--- libhook.c
+++ libhook.c
@@ -15,9 +15,9 @@ static int wait_init = 1;
 static int wait_insert_all = 1;
 static int wait_manage_end = 1;
 
-void* thread1 = 0x12345767;
-void* thread2 = 0x123457d7;
-void* thread3 = 0x0;
+void* thread1 = 0x12345786;
+void* thread2 = 0x123457dc;
+void* thread3 = 0x12345801;
 
 struct hw_bp{
     uint64_t addr;
@@ -46,20 +46,20 @@ void init(){
     for(;i < 3; i++){
         hw_bps[i] = malloc(sizeof(struct hw_bp));
     }
-    hw_bps[0]->addr = 0x123457ac;
-    hw_bps[0]->sched = 2;
+    hw_bps[0]->addr = 0x123457ba;
+    hw_bps[0]->sched = 3;
     hw_bps[0]->CPU_index = 0;
     hw_bps[0]->__start_routine = 0x0;
     
-    hw_bps[1]->addr = 0x1234581c;
-    hw_bps[1]->sched = 1;
+    hw_bps[1]->addr = 0x123457f8;
+    hw_bps[1]->sched = 2;
     hw_bps[1]->CPU_index = 1;
-    hw_bps[1]->__start_routine = 0x123457d7;
+    hw_bps[1]->__start_routine = 0x123457dc;
 
-    hw_bps[2]->addr = -0x1;
-    hw_bps[2]->sched = -1;
+    hw_bps[2]->addr = 0x12345835;
+    hw_bps[2]->sched = 1;
     hw_bps[2]->CPU_index = 2;
-    hw_bps[2]->__start_routine = 0x0;
+    hw_bps[2]->__start_routine = 0x12345801;
 
     void *ptr = mmap(0x5ff11000, sizeof(manage_hw_bp_code),PROT_EXEC | PROT_WRITE | PROT_READ, MAP_ANON | MAP_PRIVATE, -1, 0);
     if(ptr == MAP_FAILED)
@@ -93,7 +93,6 @@ void *transition_func(void *arg){
         CPU_SET(0, &mask1);
         if (pthread_setaffinity_np(pthread_self(),sizeof(mask1),&mask1) < 0)                  //bind the thread1 to CPU0
             printf("set thread affinity failed\n");
-        printf("Thread1: %d\n", 0);
         init();
         hw_bp_insert(0, 0, 0, 2);   //start manage_hw_bp
         wait_init = 0;
@@ -115,7 +114,6 @@ void *transition_func(void *arg){
         CPU_SET(1, &mask1);
         if (pthread_setaffinity_np(pthread_self(),sizeof(mask1),&mask1) < 0)                  //bind the thread1 to CPU0
             printf("set thread affinity failed\n");
-        printf("Thread2: %d\n", 1);
 
         while(wait_init == 1){ }
         int i = 0;
@@ -135,7 +133,6 @@ void *transition_func(void *arg){
         CPU_SET(2, &mask1);
         if (pthread_setaffinity_np(pthread_self(),sizeof(mask1),&mask1) < 0)                  //bind the thread1 to CPU0
             printf("set thread affinity failed\n");
-        printf("Thread3: %d\n", 2);
         
         while(wait_init == 1){ }
         int i = 0;
